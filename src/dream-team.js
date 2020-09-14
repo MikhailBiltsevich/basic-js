1524f79e3ffbca0e9731ac8572be5a34eaa531ed
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2020-09-14 22:30:04 +0300

    chore: dream-team task solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else if (members.length === 0) {
+    return null;
+  }
+
+  let regexp = /[a-z]/i;
+
+  members = members.filter(member => typeof member == 'string');
+  members = members.map(member => member.charAt(member.search(regexp)).toUpperCase());
+  members.sort();
+
+  return members.join('');
 };
